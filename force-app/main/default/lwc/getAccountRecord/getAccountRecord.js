b2f55540bc4e923e264b5b2096ea0b8cfffb5e76
AJA11 <ajaysethi184@gmail.com>
2024-05-05 14:34:23 +0530

    add Account Record Controller

---
+++ force-app/main/default/lwc/getAccountRecord/getAccountRecord.js
@@ -0,0 +1,50 @@
+import { LightningElement,wire } from 'lwc';
+import getAccountData from '@salesforce/apex/GetRecordDataController.getAccountData';
+import insertAccountData from '@salesforce/apex/GetRecordDataController.insertAccountData';
+import { refreshApex } from '@salesforce/apex';
+export default class GetAccountRecord extends LightningElement 
+
+{
+
+    accountData;
+
+    getDataFromAccount;
+   @wire(getAccountData) 
+    getData(result)
+    {
+        this.accountData=result;
+        const{error,data}=result;
+        if(data)
+        {
+            this.getDataFromAccount=data;
+            
+        }
+        else if(error)
+        {
+            console.log(error);
+        }
+    } 
+    
+
+    inserRecord()
+    {
+        let paremeterObject=
+        
+        {
+            accountName:this.template.querySelector('lightning-input[data-field="nameValue"').value,
+            phoneNumber:this.template.querySelector('lightning-input[data-field="phoneValue"').value,
+        }
+       console.log(paremeterObject);
+       console.log(paremeterObject.accountName);
+
+        insertAccountData(
+            {
+                getData:paremeterObject
+            }).then(res=>
+                {
+                    refreshApex(this.accountData);
+                    console.log(res);
+                })
+
+    }
+}
